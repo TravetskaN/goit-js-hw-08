7f6c0e65638b2d23053cb52e422570c92771575d
TravetskaN <ntravetskaya@gmail.com>
2022-01-14 16:03:34 +0200

    дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,29 +8,25 @@ const galleryItemsEl = document.querySelector('.gallery');
 const selectorGallery = images => {
   return images
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item"><a class="gallery__link" href=${original}><img class= gallery__image src="${preview}" data-source =${original} alt="${description}"></div>`;
+      return `<li><a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}" />
+        </a></li>`;
     })
     .join('');
 };
 const cardImagesMarkup = selectorGallery(galleryItems);
 galleryItemsEl.insertAdjacentHTML('afterbegin', cardImagesMarkup);
+
 galleryItemsEl.addEventListener('click', onPictureClick);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 function onPictureClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width:"800" height: "600">
-`);
-
-  instance.show();
-  window.addEventListener('keydown', onEscKeyPress);
-  function onEscKeyPress() {
-    instance.close();
-  }
+  lightbox();
 }
 
 console.log(galleryItems);
